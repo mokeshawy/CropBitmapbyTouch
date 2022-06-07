548c639989c51ab010cb8105e59283b810becaa8
mohamed <mokeshawy@gmail.com>
2022-06-07 13:23:56 +0200

    initial commit

---
+++ app/src/main/java/com/example/cropbitmapbytouch/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cropbitmapbytouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.cropbitmapbytouch.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+
+}
